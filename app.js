cf6e54a78eaf30ede0a845deb074660adb8c6952
Panadora <chenhuanhui123@gmail.com>
2020-10-02 12:03:46 +0700

    Update

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const Emitter = require("./emitter/emitter");
+//const Emitter = require("./emitter/emitter");
+const Emitter = require("events");
+
+const util = require('util');
 
 const emitter = new Emitter();
 
@@ -22,3 +25,22 @@ obj.message.call( { name: "Trần Đức Lĩnh" }, "contact", "my blog https://d
 
 obj.message.apply( { name: "Cao Xuân Viễn" }, ["info", "Đam mê về  nghệ thuật múa bài tây !"]);
 
+// Dialog Kế thừa  emitter
+function Dialog()  {
+    this.message = "\nHello World";
+}
+
+util.inherits(Dialog, Emitter);
+
+Dialog.prototype.sayhello = function() {
+    console.log(this.message);
+    this.emit("hello");
+}
+
+const dialog = new Dialog();
+
+dialog.on("hello", () => {
+    console.log("Welcome to NodeJS");
+})  
+
+dialog.sayhello();
